fa9bf5fd66b0cb0b5639d3acd0a8963a8abb39ed
Avinash <avinashraj3884@gmail.com>
2024-02-24 21:55:00 +0530

    Create your own react library and JSX

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,9 @@
 
 import Chai from"./chai"
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div>
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+// function MyApp() {
+  
+
+//     return (
+//   <div>
+//     <h1>Custom app!</h1>
+//   </div>
+//     )
+// }
+const ReactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'click me to visit google'
+}
+const anotherElement = (
+    <a href="https://google.com" target='_blanck'>Visit google</a>
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    anotherElement
  
 )
